c4230b49ad38fb8af0567cffa07e244aa446cc8c
Ryan DeBeasi <rdebeasi@redhat.com>
2017-05-12 13:20:49 -0400

    Interact with the API.

--- client/js/index.js
+++ client/js/index.js
@@ -1,50 +1,58 @@
 (function() {
-  // TODO: implement real API
-  var messages = [
-    {
-      "message": "Donation 2",
-      "amount": 2
-    },
-    {
-      "message": "Donation 3",
-      "amount": 3
-    },
-    {
-      "message": "Donation 4",
-      "amount": 4
-    },
-    {
-      "message": "Donation 5",
-      "amount": 5
-    },
-    {
-      "message": "Donation 6",
-      "amount": 6
-    },
-    {
-      "message": "Donation 7",
-      "amount": 7
-    },
-    {
-      "message": "Donation 8",
-      "amount": 8
-    },
-    {
-      "message": "Donation 9",
-      "amount": 9
-    },
-    {
-      "message": "string",
-      "amount": 999
-    }
-  ];
+  // In a larger application, we'd use ES6 modules to import Vue and Axios.
+  // For now, we're including them in script tags, making them available globally.
+  //
+  // We wrap our code in an IIFE to prevent our variables from leaking into global
+  // scope. http://benalman.com/news/2010/11/immediately-invoked-function-expression/
 
+  var apiBase = 'http://localhost:8080/v1/';
 
-  var app = new Vue({
+  // Create a viewModel, using the Vue constructor function.
+  var vm = new Vue({
     el: '#app',
     data: {
-      messages: messages
+      // Vue will update the view when these variables are changed.
+      messages: [],
+      draft: {
+        message: '',
+        card: '',
+        amount: ''
+      }
+    },
+    methods: {
+      makePayment: function() {
+        return axios.post(apiBase + 'payment-gateway', {
+          amount: this.draft.amount,
+          cardNumber: this.draft.card
+        });
+      },
+      sendMessage: function( transaction ) {
+        return axios.post(apiBase + 'donation', {
+          amount: this.draft.amount,
+          message: this.draft.message,
+          transactionId: transaction.data.transactionId
+        });
+      },
+      showSuccess: function(stuff) {
+        console.log('success', stuff);
+      },
+      onSubmit: function(event) {
+        // With a Vue method, `this` is the viewModel.
+        // console.log(typeof this.draft.amount);
+        this.makePayment()
+          .then( this.sendMessage )
+          .then( this.showSuccess )
+          .catch( function(error) { console.log(error) });
+      }
     }
   });
 
+  axios.get(apiBase + 'donation')
+    .then(function(response) {
+      vm.messages = response.data;
+      console.log(vm.messages);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
 })();
