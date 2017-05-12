056c02ee9b0c7cc7e5c9aaa99120322416d5626a
Matyas Danter <mdanter@gmail.com>
2017-05-12 13:08:28 -0400

    enabling CORS

--- spring-server/src/main/java/io/swagger/api/ApiOriginFilter.java
+++ spring-server/src/main/java/io/swagger/api/ApiOriginFilter.java
@@ -2,11 +2,17 @@ package io.swagger.api;
 
 import java.io.IOException;
 
-import javax.servlet.*;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2017-05-12T01:38:34.826Z")
+import org.springframework.stereotype.Component;
 
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2017-05-12T01:38:34.826Z")
+@Component
 public class ApiOriginFilter implements javax.servlet.Filter {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response,
